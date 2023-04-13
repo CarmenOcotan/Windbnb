e0fc099092c7a7f86f044fe9c85cc4501cbb5132
Carmen <liz.ocotan.clno@gmail.com>
2023-04-12 22:48:49 -0600

    agregando las cards y otros detalles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+import data from "./data.js";
+import dom from "./dom.js"
+
+const url = './stays.json';
+
+const lugares = dom.$('#lugares')
+
+const datos = await data.getData(url)
+
+datos.forEach(element => {
+    const card = dom.nCards(element);
+
+    lugares.appendChild(card);
+});
 
