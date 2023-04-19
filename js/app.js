8e2c2c68d3209113c3ccacbc395d9a142b2fea7a
Carmen <liz.ocotan.clno@gmail.com>
2023-04-19 03:07:22 -0600

    casiFinal

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 import data from "./data.js";
 import dom from "./dom.js"
 
-const lugares = dom.$('#lugares')
+/* const lugares = dom.$('#lugares') */
 
 const datos = await data.getData()
-/*  */
 
+const ciudades = data.cityUnica(datos)
+
+dom.allCiudades(ciudades);
 
 datos.forEach(element => {
     const card = dom.nCards(element);
@@ -13,13 +15,36 @@ datos.forEach(element => {
     lugares.appendChild(card);
 });
 
-const addCiudad = dom.$('#filtrarCiudad');
 
-addCiudad.addEventListener('click', () => {
+
+const addCiudad = [...dom.$('#lista').children];
+
+let active1 = 0;
+
+addCiudad.forEach((lista, index) => {
+
+  lista.addEventListener('click', () => {
+  
+    if (lista.classList.contains('local1')) return;
+
+    lista.classList.add('local1');
+
+    let pass = addCiudad[active1];
+
+    pass.classList.remove('local1');
+
+    active1 = index;
+
+    let filtro = lista.textContent;
+
+    const filCiudad = data.filtrar(datos, filtro);
+    console.log(filCiudad)
     
-  let filtro = addCiudad.value;
-  const filCiudad = filtro === '' ? datos : data.filtroCiudad(datos, filtro);
-  dom.nCiudad(filCiudad);
-});
+    dom.mCards(filCiudad)
+  });
+})
+dom.mCards(datos)
 
+//AGREGANDO EL CONTADOR 
 
+/* const addGuests = [...dom.$('#mostrar').children]; */
